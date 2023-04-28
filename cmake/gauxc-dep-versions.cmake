3834d69ab0b32bce3b5ac94890b6471e78595a39
Daniel Mejia-Rodriguez <daniel.mejia@pnnl.gov>
2023-04-28 14:25:02 -0700

    Update gauxc-dep-versions

--- cmake/gauxc-dep-versions.cmake
+++ cmake/gauxc-dep-versions.cmake
@@ -1,5 +1,5 @@
 set( GAUXC_LINALG_MODULES_REPOSITORY https://github.com/wavefunction91/linalg-cmake-modules.git )
-set( GAUXC_LINALG_MODULES_REVISION  89c82b78505f60657ef33b5bf38e5da059dff7f2 ) 
+set( GAUXC_LINALG_MODULES_REVISION  97d9d968917b9e60ff07033d298e8d3ffb7e82ac ) 
 
 set( GAUXC_CEREAL_REPOSITORY https://github.com/USCiLab/cereal.git )
 set( GAUXC_CEREAL_REVISION   v1.3.0 )
